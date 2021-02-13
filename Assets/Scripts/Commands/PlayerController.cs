5c82b8e6dc8ab9a956898b80f189fc9549fa299a
jeanfwts <weets.jeanfrancois@gmail.com>
2021-02-13 21:23:46 +0100

    initial commit

---
+++ Assets/Scripts/Commands/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject selectedUnit;
+    Tilemap floorTilemap;
+
+    void Awake()
+    {
+        //floorTilemap = GameObject.FindGameObjectWithTag("Floor") as Tilemap;    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+}
