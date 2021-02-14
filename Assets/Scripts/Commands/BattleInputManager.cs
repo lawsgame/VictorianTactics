f386a48f2b572c940b7f4504b01b7a9e1e6b54a0
jeanfwts <weets.jeanfrancois@gmail.com>
2021-02-14 18:18:57 +0100

    display cell pos from mouse click

---
+++ Assets/Scripts/Commands/BattleInputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleInputManager : MonoBehaviour
+{
+    private GameObject battlefield;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        battlefield = GameObject.FindGameObjectWithTag("Battlefield");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Commands/DisplaceCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaceCommand : Command 
+{
+
+
+    public void execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void undo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
